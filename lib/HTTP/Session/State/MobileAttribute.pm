b78ee35f84d8edc813f6e2106315ba89790a6745
tokuhirom <tokuhirom@gmail.com>
2010-06-06 22:51:15 +0900

    initial import

---
+++ lib/HTTP/Session/State/MobileAttribute.pm
@@ -0,0 +1,39 @@
+package HTTP::Session::State::MobileAttribute;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+HTTP::Session::State::MobileAttribute -
+
+=head1 SYNOPSIS
+
+  use HTTP::Session::State::MobileAttribute;
+
+=head1 DESCRIPTION
+
+HTTP::Session::State::MobileAttribute is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
